ae88a6c80d1dce81348079988849bbfc53f4bec2
mediaresearch-apr <mediaresearch612@gmail.com>
2025-11-07 17:16:05 +0530

    Update Automatedapp.py

--- Automatedapp.py
+++ Automatedapp.py
@@ -1803,7 +1803,7 @@ News search: All Articles: entity mentioned at least once in the article"""
                     run.font.bold = True
         #           run.font.bold = True
                     run.font.name = 'Helvetica'
-                    run.font.color.rgb = RGBColor(255, 255, 255)  # White color
+                    run.font.color.rgb = (255, 255, 255)  # White color
                     paragraph.alignment = PP_ALIGN.LEFT
                     paragraph.vertical_anchor = MSO_VERTICAL_ANCHOR.MIDDLE
             # Add title slide after the first slide
